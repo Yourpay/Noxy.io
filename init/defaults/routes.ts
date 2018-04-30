c81107d9991628e296df0fce695ec86c8ca6d57b
oscelest <oscelest@gmail.com>
2018-05-01 00:25:20 +0200

    Redoing

--- init/defaults/routes.ts
+++ init/defaults/routes.ts
@@ -3,30 +3,29 @@ import {elements, init_chain} from "../../app";
 import User from "../../objects/User";
 import * as env from "../../env.json";
 import * as jwt from "jsonwebtoken";
+import * as Promise from "bluebird";
 import * as _ from "lodash";
 
 init_chain.addPromise("route", resolve => {
   
   Application.addRoute("POST", {path: "/api/user", parameter: "/login"}, Application.auth, (request, response) => {
-    new User(request.body).validate()
+    new Promise((resolve, reject) => {
+      if (request.get("Authorization")) { return jwt.verify(request.get("Authorization"), env.tokens.jwt, (err, decoded) => !err ? resolve(decoded) : reject(err)); }
+      if (request.body.password) { return resolve(request.body); }
+      reject();
+    })
     .then(res => {
-      if (!User.generateHash(request.body.password, res.salt).equals(res.hash)) { return response.sendStatus(401); }
-      const token = jwt.sign({id: res.uuid}, env.tokens.jwt, {expiresIn: "7d"});
-      return response.json(Application.response(_.merge({jwt: token}, res.toObject())));
+      new User(res).validate()
+      .then(res => {
+        if (request.body.password && !User.generateHash(request.body.password, res.salt).equals(res.hash)) { return response.sendStatus(401); }
+        res.time_login = Date.now();
+        res.save()
+        .then(res => response.json(Application.response(jwt.sign(res.toObject(), env.tokens.jwt, {expiresIn: "7d"}))))
+        .catch(err => response.sendStatus(500));
+      })
+      .catch(err => response.sendStatus(401));
     })
     .catch(err => response.sendStatus(401));
-  });
-  
-  Application.addRoute("POST", {path: "/api/user", parameter: "/refresh"}, Application.auth, (request, response) => {
-    jwt.verify(request.get("Authorization"), env.tokens.jwt, (err, decoded) => {
-      if (err) { return response.sendStatus(401); }
-      new User(decoded).validate()
-      .then(res => {
-        const token = jwt.sign({id: decoded.id}, env.tokens.jwt, {expiresIn: "7d"});
-        return response.json(Application.response(_.merge({jwt: token}, res.toObject())));
-      })
-      .catch(err => console.log(err) || response.sendStatus(401));
-    });
   });
   
   _.each(elements, v => { Application.addElementRouter(v); });
