ee574ede36db8e482127808cef5474b896dc1a3a
oscelest <oscelest@gmail.com>
2018-07-11 22:49:05 +0200

    Merged

---
+++ plugins/admin/public/components/About.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export default class About extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+}
--- plugins/admin/public/components/App.tsx
+++ plugins/admin/public/components/App.tsx
@@ -1,4 +1,8 @@
 import * as React from "react";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Topics from "./Topics";
 
 export default class App extends React.Component<any, any> {
   constructor(props) {
@@ -7,8 +11,27 @@ export default class App extends React.Component<any, any> {
   
   render() {
     return (
-      <div>Something here</div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+      
+          <hr/>
+      
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/topics" component={Topics}/>
+        </div>
+      </Router>
     );
   }
-  
 }
